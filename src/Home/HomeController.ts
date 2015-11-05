a4576540b8e02a3e7c08d1c22d03567f010df37c
Jason McTaggart <mctaggaj@mail.uoguelph.ca>
2015-11-05 13:29:26 -0500

    Feat: Home
    Integrate Home With TileService

--- src/Home/HomeController.ts
+++ src/Home/HomeController.ts
@@ -2,18 +2,33 @@
 module App.Home {
 
     interface IHomeControllerShell extends ng.IScope{
+        tiles: Tile.ITile[];
+        clickTile(tile: Tile.ITile):void;
+    }
 
+    interface IHomeStateParams {
+        filter: string;
     }
 
     export class HomeController {
+        private tileService: Tile.TileService;
+
         public static controllerId = "HomeController";
         public static moduleId = Home.moduleId + "." + HomeController.controllerId;
 
-        public static $inject = ["$scope",Data.DataService.serviceId];
-        constructor (private $scope: IHomeControllerShell) {
-
+        public static $inject = ["$scope","$stateParams",Tile.TileService.serviceId];
+        constructor (private $scope: IHomeControllerShell, $stateParams: IHomeStateParams, tileService: Tile.TileService) {
+            this.$scope = $scope;
+            this.$scope.clickTile = (tile: Tile.ITile) => this.tileService.executeTile(tile);
+            this.tileService = tileService;
+            this.tileService.getTilesForFilter($stateParams.filter).then((tiles: Tile.ITile[]) => {
+                this.$scope.tiles = tiles;
+            }, () => {
+                // Todo: Handle failure
+            })
         }
     }
+
     // Angular module and controller registration
     angular.module(HomeController.moduleId, []).
         controller(HomeController.controllerId, HomeController)
@@ -21,7 +36,7 @@ module App.Home {
             $stateProvider.state(Home.state, {
                 templateUrl: Home.baseUrl+'home.html',
                 controller: HomeController.controllerId,
-                url: "/home"
+                url: "/home/{filter}"
             })
         }])
 }
