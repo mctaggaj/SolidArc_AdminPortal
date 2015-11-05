3adcd4debdbdda432a6c5cae22c0fe796da2904a
Jason McTaggart <mctaggaj@mail.uoguelph.ca>
2015-11-05 15:44:30 -0500

    Feat: Home
    Integrate home with TitleForFilterService

--- src/Home/HomeController.ts
+++ src/Home/HomeController.ts
@@ -4,6 +4,7 @@ module App.Home {
     interface IHomeControllerShell extends ng.IScope{
         tiles: Tile.ITile[];
         clickTile(tile: Tile.ITile):void;
+        title: string;
     }
 
     interface IHomeStateParams {
@@ -16,11 +17,16 @@ module App.Home {
         public static controllerId = "HomeController";
         public static moduleId = Home.moduleId + "." + HomeController.controllerId;
 
-        public static $inject = ["$scope","$stateParams",Tile.TileService.serviceId];
-        constructor (private $scope: IHomeControllerShell, $stateParams: IHomeStateParams, tileService: Tile.TileService) {
+        public static $inject = ["$scope","$stateParams",Tile.TileService.serviceId, Tile.TitleForFilter.TitleForFilterService.serviceId];
+        constructor (private $scope: IHomeControllerShell, $stateParams: IHomeStateParams, tileService: Tile.TileService, titleForFilterService: Tile.TitleForFilter.TitleForFilterService) {
             this.$scope = $scope;
             this.$scope.clickTile = (tile: Tile.ITile) => this.tileService.executeTile(tile);
             this.tileService = tileService;
+            titleForFilterService.getTitleForFilter($stateParams.filter).then((title: string) => {
+                this.$scope.title = title;
+            }, () => {
+                this.$scope.title = "Dashboard";
+            })
             this.tileService.getTilesForFilter($stateParams.filter).then((tiles: Tile.ITile[]) => {
                 this.$scope.tiles = tiles;
             }, () => {
@@ -30,7 +36,7 @@ module App.Home {
     }
 
     // Angular module and controller registration
-    angular.module(HomeController.moduleId, []).
+    angular.module(HomeController.moduleId, [Tile.moduleId]).
         controller(HomeController.controllerId, HomeController)
         .config(["$stateProvider", ($stateProvider: ng.ui.IStateProvider) => {
             $stateProvider.state(Home.state, {
