68ffd199167afa0316fe54129c9f067179790c5d
sea-grass <jsmith@mail.uoguelph.ca>
2015-11-10 12:27:11 -0500

    Feat: TileService
    Split tile service and mock data into separate files

---
+++ src/Tile/TileMockData.ts
@@ -0,0 +1,286 @@
+/// <reference path="TileGlobals.ts" />
+
+module App.Tile {
+  export var mockData = [
+{
+                    text: "Events",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "events"
+                        }
+                    }
+                },
+                {
+                    text: "Participants",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "participants"
+                        }
+                    }
+                },
+                {
+                    text: "Create Participant",
+                    tags: [
+                        "all",
+                        "create",
+                        "participants"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Edit Participant",
+                    tags: [
+                        "all",
+                        "edit",
+                        "participants"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Delete Participant",
+                    tags: [
+                        "all",
+                        "delete",
+                        "participants"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Add Participant to Team",
+                    tags: [
+                        "all",
+                        "add",
+                        "teams",
+                        "participants"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Remove Participant to Team",
+                    tags: [
+                        "all",
+                        "remove",
+                        "teams",
+                        "participants"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Routes",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Create Route",
+                    tags: [
+                        "all",
+                        "create",
+                        "routes"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "routes",
+                        stateParams: {
+                        }
+                    }
+                },
+                {
+                    text: "Edit Route",
+                    tags: [
+                        "all",
+                        "edit",
+                        "routes"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Delete Route",
+                    tags: [
+                        "all",
+                        "delete",
+                        "routes"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Assign Team to Route",
+                    tags: [
+                        "all",
+                        "assign",
+                        "teams",
+                        "routes"
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "routes"
+                        }
+                    }
+                },
+                {
+                    text: "Teams",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "teams"
+                        }
+                    }
+                },
+                {
+                    text: "Info",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "event"
+                        }
+                    }
+                },
+                {
+                    text: "Chat",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "chat"
+                        }
+                    }
+                },
+                {
+                    text: "Administrators",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "administrators"
+                        }
+                    }
+                },
+                {
+                    text: "Buses",
+                    tags: [
+                        "all",
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "buses"
+                        }
+                    }
+                },
+                {
+                    text: "All",
+                    tags: [
+                        ""
+                    ],
+                    textColor: "white",
+                    background: "black",
+                    action: {
+                        state: "home",
+                        stateParams: {
+                            filter: "all"
+                        }
+                    }
+                }
+  ];
+}
