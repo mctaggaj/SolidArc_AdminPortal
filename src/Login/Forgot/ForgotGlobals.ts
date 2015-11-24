b5b2ad3e8afa72d72ee059ae4e62f210a276ab22
Joe Szasz <jszasz@mail.uoguelph.ca>
2015-11-24 15:31:39 -0500

    Added a forget your password feature, and removed the main tiles from all

---
+++ src/Login/Forgot/ForgotGlobals.ts
@@ -0,0 +1,9 @@
+/// <reference path="../LoginGlobals.ts" />
+
+module App.Login.Forgot {
+
+    export var moduleId = Login.moduleId + ".Forgot";
+    export var baseUrl = Login.baseUrl + "Forgot/";
+
+    export var state = "Forgot";
+}
--- src/Login/LoginModule.ts
+++ src/Login/LoginModule.ts
@@ -1,5 +1,6 @@
 /// <reference path="LoginGlobals.ts" />
 /// <reference path="LoginController.ts" />
+/// <reference path="Forgot/ForgotModule.ts" />
 
 module App.Login {
     angular.module(Login.moduleId, App.getChildModuleIds(Login));
