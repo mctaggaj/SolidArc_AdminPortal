b72731ddcee6d3d380719341be8b88cdeb59b83b
Jason McTaggart <mctaggaj@mail.uoguelph.ca>
2015-11-26 13:19:33 -0500

    Fix(DataService): Mocks
    Rename

--- src/Data/DataGlobals.ts
+++ src/Data/DataGlobals.ts
@@ -5,8 +5,8 @@ module App.Data {
     export var moduleId = App.moduleId + ".Data";
     export var baseUrl = App.baseUrl + "Data/";
 
-    export var LS_UseLocalStorage = "SolidArc.Data.UseLocalStorage";
-    export var LS_UseLocalStorage_Events = "SolidArc.Data.UseLocalStorage.Events";
-    export var LS_UseLocalStorage_Teams = "SolidArc.Data.UseLocalStorage.Teams";
-    export var LS_UseLocalStorage_Participants = "SolidArc.Data.UseLocalStorage.Participants";
+    export var LS_UseMocks = "SolidArc.Data.UseMocks";
+    export var LS_UseMocks_Events = "SolidArc.Data.UseMocks.Events";
+    export var LS_UseMocks_Teams = "SolidArc.Data.UseMocks.Teams";
+    export var LS_UseMocks_Participants = "SolidArc.Data.UseMocks.Participants";
 }
